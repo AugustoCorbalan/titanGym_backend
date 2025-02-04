9d4af72437fe55958d1e74ede2f9f7e95e238979
unknown <auguscorbi@gmail.com>
2025-02-04 19:17:24 -0300

    Creo middleware de autenticación del token

---
+++ src/routes/middleware/authenticateToken.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    const {JWT_SECRET} = process.env;
+    if (!token) {
+        return res.status(401).json({ error: 'Token requerido' });
+    }
+
+    // Quitamos el "Bearer " si existe, dejando solo el token
+    const tokenWithoutBearer = token.startsWith('Bearer ') ? token.slice(7) : token;
+
+    jwt.verify(tokenWithoutBearer, JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Token no válido' });
+        }
+        // Guardamos el user en la solicitud para usarlo después
+        req.user = user;
+        next();  // Continuamos al siguiente middleware o ruta
+    });
+};
