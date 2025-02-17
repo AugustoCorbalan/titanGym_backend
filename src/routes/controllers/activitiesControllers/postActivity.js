86248adefa5a11a6135e3b8e49cee9733dfbbbc0
AugustoCorbalan <auguscorbi@gmail.com>
2025-02-17 17:41:00 -0300

    Elimino conosle.log()

--- src/routes/controllers/activitiesControllers/postActivity.js
+++ src/routes/controllers/activitiesControllers/postActivity.js
@@ -4,10 +4,7 @@ import {loadImage} from '../../functionsAux/cloudinaryFunctions.js';
 const postActivity = async (req, res)=>{
     try {
         const data = JSON.parse(req.body.data);
-        console.log(req.files);
-        console.log(data);
         const images = await loadImage(req.files); //Cargo imagenes a Cloudinary
-        console.log("images", images);
         const newData = {...data, icon: images[0]};
         const result = await Activity.create(newData);
         res.status(200).send(result);
