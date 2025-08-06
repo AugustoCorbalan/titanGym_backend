330bf23d3563b03f384a8dd21c4c3d8975ed538a
AugustoCorbalan <auguscorbi@gmail.com>
2025-08-06 19:05:33 -0300

    SSL sequelize

--- database/db.js
+++ database/db.js
@@ -6,7 +6,13 @@ const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST} = process.env;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // necesario para Render y algunas DB externas
+        }
+    }
 });
 
 export default sequelize;
