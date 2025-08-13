c7e783b60dd21b47d8a54ff3ed8902e5f822855f
AugustoCorbalan <auguscorbi@gmail.com>
2025-08-13 17:58:28 -0300

    Elimino notifications de getUser

--- src/routes/controllers/userControllers/getUser.js
+++ src/routes/controllers/userControllers/getUser.js
@@ -5,17 +5,10 @@ const getUser = async (req, res)=>{
     try {
         const googleId = req.user.id;
         const result = await User.findByPk(googleId,{   
-            include: [
+            include:
             {
                 model: Activity // Incluye las actividades relacionadas
-            },
-            {
-                model: Notifications,
-                separate: true, // Hace una consulta aparte para Notifications
-                limit: 5,       // Trae solo las últimas 5
-                order: [['createdAt', 'DESC']] // Ordena de más nuevas a más antiguas
             }
-        ]
         })
         res.status(200).send(result);
     } catch (error) {
